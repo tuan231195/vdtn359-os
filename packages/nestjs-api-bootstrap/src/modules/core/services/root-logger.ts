f691c84a30d28d9203fea6b5adb4fbde325c87ab
Tuan Nguyen <tuan@worldsportaction.com>
2023-01-23 22:35:04 +1100

    docs(changeset): fix pino pretty

--- packages/nestjs-api-bootstrap/src/modules/core/services/root-logger.ts
+++ packages/nestjs-api-bootstrap/src/modules/core/services/root-logger.ts
@@ -4,6 +4,7 @@ import { BOOTSTRAP_OPTIONS_TOKEN } from 'src/modules/core/tokens';
 import pino from 'pino';
 import serializers from 'pino-std-serializers';
 import { DefaultLoggerService } from 'src/modules/core/services/default-logger-service';
+import pinoPretty from 'pino-pretty';
 
 @Injectable()
 export class RootLogger extends DefaultLoggerService {
@@ -11,27 +12,30 @@ export class RootLogger extends DefaultLoggerService {
 		@Inject(BOOTSTRAP_OPTIONS_TOKEN)
 		private readonly bootstrapOptions: BootstrapOptions
 	) {
-		const isProd = process.env.NODE_ENV === 'production';
-		const logger = pino({
-			name: bootstrapOptions.name,
-			version: bootstrapOptions.version,
-			level: process.env.LOG_LEVEL ?? 'info',
-			messageKey: 'message',
-			paths: ['req.headers.authorization', "req.headers['x-api-key']"],
-			serializers,
-			formatters: {
-				level: (label: string) => ({ level: label }),
-			},
-			...(!isProd && {
-				transport: {
-					target: 'pino-pretty',
-					options: {
-						colorize: true,
-						messageKey: 'message',
-					},
+		const colorizedLogs = process.env.ENABLE_COLORIZED_LOGS === 'true';
+		const stream = colorizedLogs
+			? pinoPretty({
+					colorize: true,
+					messageKey: 'message',
+			  })
+			: undefined;
+		const logger = pino(
+			{
+				name: bootstrapOptions.name,
+				version: bootstrapOptions.version,
+				level: process.env.LOG_LEVEL ?? 'info',
+				messageKey: 'message',
+				paths: [
+					'req.headers.authorization',
+					"req.headers['x-api-key']",
+				],
+				serializers,
+				formatters: {
+					level: (label: string) => ({ level: label }),
 				},
-			}),
-		});
+			},
+			stream as any
+		);
 		super(logger);
 	}
 
--- packages/nestjs-bootstrap/src/modules/core/services/root-logger.ts
+++ packages/nestjs-bootstrap/src/modules/core/services/root-logger.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import { BootstrapOptions } from 'src/modules/core/interface';
 import { BOOTSTRAP_OPTIONS_TOKEN } from 'src/modules/core/tokens';
 import pino from 'pino';
+import pinoPretty from 'pino-pretty';
 import serializers from 'pino-std-serializers';
 import { DefaultLoggerService } from 'src/modules/core/services/default-logger-service';
 
@@ -11,27 +12,30 @@ export class RootLogger extends DefaultLoggerService {
 		@Inject(BOOTSTRAP_OPTIONS_TOKEN)
 		private readonly bootstrapOptions: BootstrapOptions
 	) {
-		const isProd = process.env.NODE_ENV === 'production';
-		const logger = pino({
-			name: bootstrapOptions.name,
-			version: bootstrapOptions.version,
-			level: process.env.LOG_LEVEL ?? 'info',
-			messageKey: 'message',
-			paths: ['req.headers.authorization', "req.headers['x-api-key']"],
-			serializers,
-			formatters: {
-				level: (label: string) => ({ level: label }),
-			},
-			...(!isProd && {
-				transport: {
-					target: 'pino-pretty',
-					options: {
-						colorize: true,
-						messageKey: 'message',
-					},
+		const colorizedLogs = process.env.ENABLE_COLORIZED_LOGS === 'true';
+		const stream = colorizedLogs
+			? pinoPretty({
+					colorize: true,
+					messageKey: 'message',
+			  })
+			: undefined;
+		const logger = pino(
+			{
+				name: bootstrapOptions.name,
+				version: bootstrapOptions.version,
+				level: process.env.LOG_LEVEL ?? 'info',
+				messageKey: 'message',
+				paths: [
+					'req.headers.authorization',
+					"req.headers['x-api-key']",
+				],
+				serializers,
+				formatters: {
+					level: (label: string) => ({ level: label }),
 				},
-			}),
-		});
+			},
+			stream as any
+		);
 		super(logger);
 	}
 
